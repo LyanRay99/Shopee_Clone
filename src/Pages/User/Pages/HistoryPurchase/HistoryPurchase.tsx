e6f2f81ae25ee5610755aaf378d2f786e9d62d94
Tai Marce <mttai161099@gmail.com>
2023-03-04 22:13:13 +0700

    code ui pages User

--- src/Pages/User/Pages/HistoryPurchase/HistoryPurchase.tsx
+++ src/Pages/User/Pages/HistoryPurchase/HistoryPurchase.tsx
@@ -1,5 +1,50 @@
-import React from 'react'
+//* Library
+import { Link } from 'react-router-dom'
+
+//* Utils
+import { formatCurrency } from 'src/Utils/formatCurrency'
+import { generateNameId } from 'src/Utils/customUrl'
 
 export default function HistoryPurchase() {
-  return <div>HistoryPurchase</div>
+  return (
+    <div>
+      <div className='overflow-x-auto'>
+        <div className='min-w-[700px]'>
+          {/* <div className='sticky top-0 flex rounded-t-sm shadow-sm'>{purchaseTabsLink}</div> */}
+          <div>
+            {/* {purchasesInCart?.map((purchase) => (
+              <div key={purchase._id} className='mt-4 rounded-sm border-black/10 bg-white p-6 text-gray-800 shadow-sm'>
+                <Link
+                  to={`${path.home}${generateNameId({ name: purchase.product.name, id: purchase.product._id })}`}
+                  className='flex'
+                >
+                  <div className='flex-shrink-0'>
+                    <img className='h-20 w-20 object-cover' src={purchase.product.image} alt={purchase.product.name} />
+                  </div>
+                  <div className='ml-3 flex-grow overflow-hidden'>
+                    <div className='truncate'>{purchase.product.name}</div>
+                    <div className='mt-3'>x{purchase.buy_count}</div>
+                  </div>
+                  <div className='ml-3 flex-shrink-0'>
+                    <span className='truncate text-gray-500 line-through'>
+                      ₫{formatCurrency(purchase.product.price_before_discount)}
+                    </span>
+                    <span className='ml-2 truncate text-orange'>₫{formatCurrency(purchase.product.price)}</span>
+                  </div>
+                </Link>
+                <div className='flex justify-end'>
+                  <div>
+                    <span>Tổng giá tiền</span>
+                    <span className='ml-4 text-xl text-orange'>
+                      ₫{formatCurrency(purchase.product.price * purchase.buy_count)}
+                    </span>
+                  </div>
+                </div>
+              </div>
+            ))} */}
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
--- src/Pages/User/Pages/Profile/Profile.tsx
+++ src/Pages/User/Pages/Profile/Profile.tsx
@@ -1,5 +1,73 @@
-import React from 'react'
-
 export default function Profile() {
-  return <div>profile</div>
+  return (
+    <div className='rounded-sm bg-white px-2 pb-10 shadow md:px-7 md:pb-20'>
+      <div className='border-b border-b-gray-200 py-6'>
+        <h1 className='text-lg font-medium capitalize text-gray-900'>Hồ Sơ Của Tôi</h1>
+        <div className='mt-1 text-sm text-gray-700'>Quản lý thông tin hồ sơ để bảo mật tài khoản</div>
+      </div>
+      {/* <FormProvider {...methods}>
+        <form className='mt-8 flex flex-col-reverse md:flex-row md:items-start' onSubmit={onSubmit}>
+          <div className='mt-6 flex-grow md:mt-0 md:pr-12'>
+            <div className='flex flex-col flex-wrap sm:flex-row'>
+              <div className='truncate pt-3 capitalize sm:w-[20%] sm:text-right'>Email</div>
+              <div className='sm:w-[80%] sm:pl-5'>
+                <div className='pt-3 text-gray-700'>{profile?.email}</div>
+              </div>
+            </div>
+            <Info />
+            <div className='mt-2 flex flex-col flex-wrap sm:flex-row'>
+              <div className='truncate pt-3 capitalize sm:w-[20%] sm:text-right'>Địa chỉ</div>
+              <div className='sm:w-[80%] sm:pl-5'>
+                <Input
+                  classNameInput='w-full rounded-sm border border-gray-300 px-3 py-2 outline-none focus:border-gray-500 focus:shadow-sm'
+                  register={register}
+                  name='address'
+                  placeholder='Địa chỉ'
+                  errorMessage={errors.address?.message}
+                />
+              </div>
+            </div>
+            <Controller
+              control={control}
+              name='date_of_birth'
+              render={({ field }) => (
+                <DateSelect
+                  errorMessage={errors.date_of_birth?.message}
+                  value={field.value}
+                  onChange={field.onChange}
+                />
+              )}
+            />
+            <div className='mt-2 flex flex-col flex-wrap sm:flex-row'>
+              <div className='truncate pt-3 capitalize sm:w-[20%] sm:text-right' />
+              <div className='sm:w-[80%] sm:pl-5'>
+                <Button
+                  className='flex h-9 items-center rounded-sm bg-orange px-5 text-center text-sm text-white hover:bg-orange/80'
+                  type='submit'
+                >
+                  Lưu
+                </Button>
+              </div>
+            </div>
+          </div>
+          <div className='flex justify-center md:w-72 md:border-l md:border-l-gray-200'>
+            <div className='flex flex-col items-center'>
+              <div className='my-5 h-24 w-24'>
+                <img
+                  src={previewImage || getAvatarUrl(avatar)}
+                  alt=''
+                  className='h-full w-full rounded-full object-cover'
+                />
+              </div>
+              <InputFile onChange={handleChangeFile} />
+              <div className='mt-3 text-gray-400'>
+                <div>Dụng lượng file tối đa 1 MB</div>
+                <div>Định dạng:.JPEG, .PNG</div>
+              </div>
+            </div>
+          </div>
+        </form>
+      </FormProvider> */}
+    </div>
+  )
 }
